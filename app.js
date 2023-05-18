0a940ea3fffba8333bdb1781a3b9796c0e921f67
angelcepeda <jangelcepeda@protonmail.com>
2023-05-17 18:20:59 -0700

    unprotected routes

--- app.js
+++ app.js
@@ -58,8 +58,7 @@ app.use(cors({
 
 //protecion de rutas
 
-app.use(expressjwt({secret:jwtKey,algorithms:['HS256']}))
-   .unless({path:["/","/login"]})
+app.use(expressjwt({secret:jwtKey,algorithms:['HS256']})).unless({path:["/","/login"]});
 
 
 //declaración de rutas
